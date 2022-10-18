bd36a7249e97bb38b15d10aaeae63c08d4485ea6
Sunil Singh <sunil@redpin.com.au>
2022-10-18 14:44:09 +1100

    post types

--- web/app/themes/sunil-portfolio/inc/register-post-types.php
+++ web/app/themes/sunil-portfolio/inc/register-post-types.php
@@ -34,6 +34,9 @@ function sunil_portfolio_services_register_post_type() {
 		'can_export'          => true,
 		'rewrite_no_front'    => false,
 		'show_in_menu'        => true,
+		'show_in_graphql' 	  => true,
+		'graphql_single_name' => 'Service',
+        'graphql_plural_name' => 'Services',
 		'menu_position'       => 20,
 		'menu_icon'           => 'dashicons-heart',
 		'supports' => [
@@ -48,4 +51,62 @@ function sunil_portfolio_services_register_post_type() {
 	];
 
 	register_post_type( 'service', $args );
+}
+
+
+add_action( 'init', 'projects' );
+function projects() {
+	$args = [
+		'label'  => esc_html__( 'Projects', 'text-domain' ),
+		'labels' => [
+			'menu_name'          => esc_html__( 'Projects', 'sunil-portfolio' ),
+			'name_admin_bar'     => esc_html__( 'Project', 'sunil-portfolio' ),
+			'add_new'            => esc_html__( 'Add Project', 'sunil-portfolio' ),
+			'add_new_item'       => esc_html__( 'Add new Project', 'sunil-portfolio' ),
+			'new_item'           => esc_html__( 'New Project', 'sunil-portfolio' ),
+			'edit_item'          => esc_html__( 'Edit Project', 'sunil-portfolio' ),
+			'view_item'          => esc_html__( 'View Project', 'sunil-portfolio' ),
+			'update_item'        => esc_html__( 'View Project', 'sunil-portfolio' ),
+			'all_items'          => esc_html__( 'All Projects', 'sunil-portfolio' ),
+			'search_items'       => esc_html__( 'Search Projects', 'sunil-portfolio' ),
+			'parent_item_colon'  => esc_html__( 'Parent Project', 'sunil-portfolio' ),
+			'not_found'          => esc_html__( 'No Projects found', 'sunil-portfolio' ),
+			'not_found_in_trash' => esc_html__( 'No Projects found in Trash', 'sunil-portfolio' ),
+			'name'               => esc_html__( 'Projects', 'sunil-portfolio' ),
+			'singular_name'      => esc_html__( 'Project', 'sunil-portfolio' ),
+		],
+		'public'              => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'show_ui'             => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'show_in_rest'        => true,
+		'capability_type'     => 'post',
+		'hierarchical'        => true,
+		'has_archive'         => true,
+		'query_var'           => true,
+		'can_export'          => true,
+		'rewrite_no_front'    => false,
+		'show_in_menu'        => true,
+		'menu_position'       => 20,
+		'show_in_graphql' 	  => true,
+		'graphql_single_name' => 'Project',
+        'graphql_plural_name' => 'Projects',
+		'menu_icon'           => 'dashicons-megaphone',
+		'supports' => [
+			'title',
+			'editor',
+			'author',
+			'thumbnail',
+			'custom-fields',
+			'page-attributes',
+		],
+		'taxonomies' => [
+			'category',
+		],
+		'rewrite' => true
+	];
+
+	register_post_type( 'project', $args );
 }
