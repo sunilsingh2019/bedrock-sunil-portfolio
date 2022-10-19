8c8e1845ac3df2760a6796f7229a8c2d6aec5bea
Sunil Singh <sunil@redpin.com.au>
2022-10-19 14:57:31 +1100

    plugin

---
+++ web/app/plugins/wp-graphql-gravity-forms/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '80169e8e7b6dd4d8f5841c15859d2a687d63dded',
+        'name' => 'harness-software/wp-graphql-gravity-forms',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'harness-software/wp-graphql-gravity-forms' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '80169e8e7b6dd4d8f5841c15859d2a687d63dded',
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v4.13',
+            'version' => '4.13.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'reference' => '6eb27a6911e0e0880d09e5b11f577d3f688f7da7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
