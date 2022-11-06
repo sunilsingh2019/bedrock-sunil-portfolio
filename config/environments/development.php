026865705fc07bf0af214eb86d8028e8684ad5ef
Sunil Singh <sunil@redpin.com.au>
2022-11-06 21:37:14 +1100

    seo

--- config/environments/development.php
+++ config/environments/development.php
@@ -8,12 +8,12 @@ use function Env\env;
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', false);
-Config::define('WP_DEBUG_DISPLAY', true);
-Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
-Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
-Config::define('SCRIPT_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? false);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', false);
+Config::define('SCRIPT_DEBUG', false);
 Config::define('DISALLOW_INDEXING', true);
-Config::define( 'GRAPHQL_DEBUG', true );
+Config::define( 'GRAPHQL_DEBUG', false );
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 
 ini_set('display_errors', '1');
